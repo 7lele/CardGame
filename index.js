da75c3df330c63593eea3f74f7b8adfd835bc811
7lele <leoarn100@gmail.com>
2025-01-08 14:51:15 +0100

    Inital commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const logo = document.getElementById("userlogo");
+const Coins = document.getElementById("Coins");
+const Credits = document.getElementById("Credits");
+const CardsNumberPossessed = document.getElementById("CardsNumberPossessed");
+
+Coins.style.display = "none";
+Credits.style.display = "none";
+CardsNumberPossessed.style.display = "none";
+logo.style.display = "none";
